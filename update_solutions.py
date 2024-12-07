338010141220ca6708d056133534537d3194aaa9
Hamish <133548095+Hamster45105@users.noreply.github.com>
2024-12-07 13:17:11 +1100

    Always gather all solutions

--- update_solutions.py
+++ update_solutions.py
@@ -1,6 +1,5 @@
 from datetime import date, timedelta
 import json
-import os
 import requests
 
 # Initialize solutions as empty dictionaries
@@ -8,56 +7,38 @@ wordle_solutions = {}
 connections_solutions = {}
 strands_solutions = {}
 
-# Load existing Wordle solutions if wordle_solutions.json exists
-if os.path.exists('./solutions/wordle_solutions.json'):
-    with open('./solutions/wordle_solutions.json', 'r', encoding='utf-8') as f:
-        wordle_solutions = json.load(f)
-
-# Load existing Connections solutions if connections_solutions.json exists
-if os.path.exists('./solutions/connections_solutions.json'):
-    with open('./solutions/connections_solutions.json', 'r', encoding='utf-8') as f:
-        connections_solutions = json.load(f)
-
-# Load existing Strands solutions if strands_solutions.json exists
-if os.path.exists('./solutions/strands_solutions.json'):
-    with open('./solutions/strands_solutions.json', 'r', encoding='utf-8') as f:
-        strands_solutions = json.load(f)
-
 # Function to fetch Wordle solution
 def fetch_wordle_solution(input_date):
-    if input_date not in wordle_solutions:
-        response = requests.get(f'https://www.nytimes.com/svc/wordle/v2/{input_date}.json')
-        solution = response.json()['solution']
-        wordle_solutions[input_date] = solution
-        print(f'Fetched Wordle solution for {input_date}')
+    response = requests.get(f'https://www.nytimes.com/svc/wordle/v2/{input_date}.json')
+    solution = response.json()['solution']
+    wordle_solutions[input_date] = solution
+    print(f'Fetched Wordle solution for {input_date}')
 
 # Function to fetch Connections solution
 def fetch_connections_solution(input_date):
-    if input_date not in connections_solutions:
-        response = requests.get(f'https://www.nytimes.com/svc/connections/v2/{input_date}.json')
-        data = response.json()
-        solution = {}
-        categories = data['categories']
-        for category in categories:
-            title = category['title']
-            cards = category['cards']
-            words = [card['content'] for card in cards]
-            solution[title] = words
-        connections_solutions[input_date] = solution
-        print(f'Fetched Connections solution for {input_date}')
+    response = requests.get(f'https://www.nytimes.com/svc/connections/v2/{input_date}.json')
+    data = response.json()
+    solution = {}
+    categories = data['categories']
+    for category in categories:
+        title = category['title']
+        cards = category['cards']
+        words = [card['content'] for card in cards]
+        solution[title] = words
+    connections_solutions[input_date] = solution
+    print(f'Fetched Connections solution for {input_date}')
 
 # Function to fetch Strands solution
 def fetch_strands_solution(input_date):
-    if input_date not in strands_solutions:
-        response = requests.get(f'https://www.nytimes.com/svc/strands/v2/{input_date}.json')
-        data = response.json()
-        solution = {
-            'clue': data['clue'],
-            'spangram': data['spangram'],
-            'themeWords': data['themeWords']
-        }
-        strands_solutions[input_date] = solution
-        print(f'Fetched Strands solution for {input_date}')
+    response = requests.get(f'https://www.nytimes.com/svc/strands/v2/{input_date}.json')
+    data = response.json()
+    solution = {
+        'clue': data['clue'],
+        'spangram': data['spangram'],
+        'themeWords': data['themeWords']
+    }
+    strands_solutions[input_date] = solution
+    print(f'Fetched Strands solution for {input_date}')
 
 # Fetch solutions from start date
 wordle_start_date = date(2021, 6, 19)
